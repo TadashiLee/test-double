e5ce6aaf5ef52ae77cef9b08b1ed12952c82d397
TadashiLee <584638605@qq.com>
2020-09-15 20:18:00 +0800

    [wenchang.li]feat:use mockdorrpanel to test seuritycenter.close()

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,19 +1,30 @@
 package mock;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    @Mock
+    private DoorPanel mockDoorPanel;
 
-    @BeforeEach
-    public void setUp() {
-    }
+    @InjectMocks
+    private SecurityCenter securityCenter;
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+
+        verify(mockDoorPanel, times(1)).close();
     }
 }
