9798d7182fd531c5165668ded250b250f3752782
TadashiLee <584638605@qq.com>
2020-09-16 09:00:43 +0800

    [wenchang.li]feat:add @ExtendWith(MockitoExtension.class) to fix the problem

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,14 +2,17 @@ package stub;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
@@ -22,10 +25,8 @@ public class GradeServiceTest {
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
         long studentId = 1;
-//        doReturn(Arrays.asList(80.0, 90.0, 100.0))
-//                .when(mockGradeSystem).gradesFor(studentId);
+
         when(mockGradeSystem.gradesFor(studentId))
                 .thenReturn(Arrays.asList(80.0, 90.0, 100.0));
 
